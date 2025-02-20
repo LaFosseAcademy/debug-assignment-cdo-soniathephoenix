ba87eb2e85bf71ca27885e8d7a0889e9d112a87c
Sonia Pires <157916578+soniathephoenix@users.noreply.github.com>
2025-02-20 11:52:05 +0000

    Add files via upload

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,7 +2,7 @@ resource "aws_default_vpc" "default" {
 
 }
 
-data "aws_subnets" "default_" {
+data "aws_subnets" "default_ec2" {
     filter {
         name = "vpc-id"
         values = [aws_default_vpc.default.id]
